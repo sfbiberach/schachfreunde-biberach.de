ec84c306f2cb3de79e9c48447d40b03b717479bb
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-12-03 15:11:52 +0100

    feat: add favicon

--- app.vue
+++ app.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 useHead({
   title: 'Schachfreunde Heilbronn-Biberach 1978 e.V.',
+  titleTemplate: '%s | Schachfreunde Heilbronn-Biberach 1978 e.V.',
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'msapplication-TileColor', content: '#fff' },
+    { name: 'theme-color', content: '#fff' },
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-tourch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#d8201f' },
   ],
   htmlAttrs: {
     lang: 'de',
