ecd68ab52ce00a207c4b5e29115e415599dc4e83
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2023-12-09 20:01:48 +0100

    chore: delete unused styles and pages

--- app.config.ts
+++ app.config.ts
@@ -3,28 +3,4 @@ export default defineAppConfig({
     primary: 'appRed',
     gray: 'cool',
   },
-  button: {
-    rounded: 'rounded-full',
-    default: {
-      size: 'md',
-      color: 'black',
-    },
-  },
-  input: {
-    rounded: 'rounded-full',
-  },
-  header: {
-    wrapper: 'lg:border-b-0 lg:top-4 lg:mb-8 backdrop-blur-none bg-transparent lg:mx-4',
-    container: 'lg:rounded-full bg-white lg:shadow',
-  },
-  footer: {
-    wrapper: 'bg-black dark',
-    top: {
-      container: 'py-8 lg:py-16',
-    },
-    bottom: {
-      wrapper: 'border-t border-gray-200 dark:border-gray-800',
-      container: 'lg:py-8',
-    },
-  },
 })
