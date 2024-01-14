a37fcca5a9b6f2abe4b6c9b622d6e37d6f0b1937
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-01-14 23:41:16 +0100

    fix: navigation error

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 const { page } = useContent()
-const headline = computed(() => findPageHeadline(page.value))
+const headline = computed(() => page?.value?._dir ? findPageHeadline(page?.value) : '')
 </script>
 
 <template>
-  <UMain>
+  <UMain :class="page?.ui?.wrapper" class="break-words">
     <UContainer :ui="{ padding: page?.container ? undefined : '', constrained: page?.container ? undefined : '' }">
-      <UPageHeader v-if="page.showHeader !== false" :title="page.title" :description="page.description" :links="page.links" :headline="headline" />
-      <slot />
+      <UPageHeader v-if="page?.showHeader !== false" :title="page?.title" :description="page?.description" :links="page?.links" :headline="headline" />
+      <UPageBody :prose="page?.prose !== false" class="pb-32" :class="[page?.ui?.body]">
+        <slot />
+      </UPageBody>
     </UContainer>
   </UMain>
 </template>
--- pages/[...slug].vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <NuxtLayout>
-    <UPage>
-      <UPageBody prose>
-        <ContentDoc />
-      </UPageBody>
-    </UPage>
-  </NuxtLayout>
-</template>
