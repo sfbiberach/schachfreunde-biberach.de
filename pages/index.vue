8a28abcb5fbe7572766f8bbf893af37a11cfb744
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-02-04 04:02:05 +0100

    feat: add Tetris component

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+</script>
+
+<template>
+  <div v-if="page">
+    <ULandingSection v-bind="page.hero" class="relative">
+      <template #title>
+        <span class="z-[1]" v-html="page.hero?.title" />
+      </template>
+
+      <template #description>
+        <span class="z-[1]" v-html="page.hero?.description" />
+      </template>
+
+      <NuxtImg src="/assets/blog/20170101.dvm-u14-2016/gruppenfoto.jpg" class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700 z-[1]" />
+      <ClientOnly>
+        <Tetris />
+      </ClientOnly>
+    </ULandingSection>
+
+    <ULandingSection v-bind="page.blog">
+      <BlogList />
+    </ULandingSection>
+  </div>
+</template>
+
+<style>
+
+</style>
