2ef4d45a6aa5aa0c1185c72b143b28a23635fdf4
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-03-02 02:09:15 +0100

    fix: prevent crash when author is not found

--- composables/blog.ts
+++ composables/blog.ts
@@ -6,7 +6,7 @@ export async function useAuthors(names: string[] | string | object[] | object | 
 
   const authorPromises = normalizedAuthors.map((name) => {
     if (typeof name === 'string')
-      return useAsyncData(`users/${name}`, () => queryContent('_users').where({ name }).findOne()).then(user => user.data.value)
+      return useAsyncData(`users/${name}`, () => queryContent('_users').where({ name }).findOne()).then(user => user?.data?.value || {})
 
     return Promise.resolve(name)
   })
