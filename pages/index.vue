6d2a2a5b0d8c0545fe61bd9ebf76163a82332f7e
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-03-03 15:22:19 +0100

    feat: change hero images

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@ const { data: page } = await useAsyncData('index', () => queryContent('/').findO
 const images = [
   { class: 'h-8' },
   { src: '/assets/blog/20180704.bw-mannschaftsfinale-u16/1561.jpg' },
-  { src: '/assets/blog/20180704.bw-mannschaftsfinale-u16/1561.jpg' },
+  { src: '/assets/home/1690827815.jpg' },
 ]
 </script>
 
@@ -20,7 +20,7 @@ const images = [
           <span v-html="page.hero?.description" />
         </template>
 
-        <UPageColumns :style="{ columns: images.length > 1 ? 2 : 1 }">
+        <UPageColumns class="hidden lg:block" :style="{ columns: images.length > 1 ? 2 : 1 }">
           <template v-for="(image, _index) in images" :key="_index">
             <NuxtImg v-if="image.src" v-bind="image" class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700" />
             <div v-else v-bind="image" />
