30f91f047f22ef6a3cc78a8cae7dc6e088c9d642
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-03-13 17:55:32 +0100

    feat(blog): use page header instead of landing section

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,11 +1,5 @@
-<script lang="ts" setup>
-const { data: page } = await useAsyncData('/blog', () => queryContent('/blog').findOne())
-</script>
-
 <template>
   <NuxtLayout>
-    <ULandingSection v-bind="page?.blog">
-      <BlogList />
-    </ULandingSection>
+    <BlogList />
   </NuxtLayout>
 </template>
