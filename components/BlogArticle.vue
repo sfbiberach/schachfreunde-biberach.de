5c59a4cb2ad24ceacd476acc972a5cf6fd037d63
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-03-30 17:30:21 +0100

    Revert "Merge pull request #13 from sfbiberach/refactor/blog-query"
    This reverts commit 420a67b59ef6822fd95a39cb712c4dcc6196c665, reversing
    changes made to 16d8b6226d01fce78dafb04ec8cdce699138257c.

---
+++ components/BlogArticle.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import type { BlogArticle } from '~/types'
+
+const props = defineProps<{
+  path: string
+}>()
+
+const url = useRequestURL()
+const { copy } = useCopyToClipboard()
+
+const { data: article } = await useFetch<BlogArticle>(`/api/blog/${props.path}`)
+
+if (!article.value)
+  throw createError({ statusCode: 404, statusMessage: 'Post not found', fatal: false })
+
+const title = article.value.head?.title || article.value.title
+const description = article.value.head?.description || article.value.description
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+})
+
+// if (post.value.image?.src) {
+//   const site = useSiteConfig()
+
+//   useSeoMeta({
+//     ogImage: joinURL(site.url, post.value.image.src),
+//     twitterImage: joinURL(site.url, post.value.image.src),
+//   })
+// }
+// else {
+//   defineOgImage({
+//     component: 'Saas',
+//     title,
+//     description,
+//     headline: 'Blog',
+//   })
+// }
+
+function copyLink() {
+  copy(`${url.origin}${article.value?._path}`, { title: 'In die Zwischenablage kopiert' })
+}
+</script>
+
+<template>
+  <NuxtLayout name="article" :container="true" :authors="article?.authors">
+    <UPage v-if="article">
+      <UPageBody prose>
+        <ContentRenderer v-if="article && article.body" :value="article" />
+
+        <div class="flex items-center justify-between mt-12 not-prose">
+          <UButton icon="i-ph-arrow-left" color="primary" variant="ghost" to="/blog">
+            Zurück zum Blog
+          </UButton>
+          <div class="flex justify-end items-center gap-1.5">
+            <UButton icon="i-ph-link-simple-duotone" v-bind="($ui.button.secondary as any)" @click="copyLink">
+              URL kopieren
+            </UButton>
+          </div>
+        </div>
+
+        <hr v-if="article.surround?.length">
+
+        <UContentSurround :surround="article.surround" />
+      </UPageBody>
+
+      <template #right>
+        <UContentToc v-if="article.body && article.body.toc" :links="article.body.toc.links" />
+      </template>
+    </UPage>
+  </NuxtLayout>
+</template>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,73 +1,18 @@
 <script setup lang="ts">
-import type { BlogArticle } from '~/types'
-
 const route = useRoute()
+const queryPage = route.query.page
+const routePaths = route.path.split('/').filter(Boolean)
+const id = routePaths.at(-1) || ''
 
-const url = useRequestURL()
-const { copy } = useCopyToClipboard()
-
-const { data: article } = await useFetch<BlogArticle>(`/api/blog/${route.path}`)
-
-if (!article.value)
-  throw createError({ statusCode: 404, statusMessage: 'Post not found', fatal: false })
-
-const title = article.value.head?.title || article.value.title
-const description = article.value.head?.description || article.value.description
-
-useSeoMeta({
-  title,
-  ogTitle: title,
-  description,
-  ogDescription: description,
-})
-
-// if (post.value.image?.src) {
-//   const site = useSiteConfig()
-
-//   useSeoMeta({
-//     ogImage: joinURL(site.url, post.value.image.src),
-//     twitterImage: joinURL(site.url, post.value.image.src),
-//   })
-// }
-// else {
-//   defineOgImage({
-//     component: 'Saas',
-//     title,
-//     description,
-//     headline: 'Blog',
-//   })
-// }
-
-function copyLink() {
-  copy(`${url.origin}${article.value?._path}`, { title: 'In die Zwischenablage kopiert' })
-}
+const idNumber = routePaths.length < 2 ? 1 : Number.parseInt(id || '1')
+const page = Number.isNaN(idNumber) || (idNumber.toString() !== id && routePaths.length > 1) ? id : idNumber
 </script>
 
 <template>
-  <NuxtLayout name="article" :container="true" :authors="article?.authors">
-    <UPage v-if="article">
-      <UPageBody prose>
-        <ContentRenderer v-if="article && article.body" :value="article" />
-
-        <div class="flex items-center justify-between mt-12 not-prose">
-          <UButton icon="i-ph-arrow-left" color="primary" variant="ghost" to="/blog">
-            Zurück zum Blog
-          </UButton>
-          <div class="flex justify-end items-center gap-1.5">
-            <UButton icon="i-ph-link-simple-duotone" v-bind="($ui.button.secondary as any)" @click="copyLink">
-              URL kopieren
-            </UButton>
-          </div>
-        </div>
-
-        <hr v-if="article.surround?.length">
-
-        <UContentSurround :surround="article.surround" />
-      </UPageBody>
-
-      <template #right>
-        <UContentToc v-if="article.body && article.body.toc" :links="article.body.toc.links" />
-      </template>
-    </UPage>
-  </NuxtLayout>
+  <template v-if="routePaths.length < 2 || typeof page === 'number'">
+    <BlogList :page="((queryPage || 1) as number)" />
+  </template>
+  <template v-else>
+    <BlogArticle :path="page" />
+  </template>
 </template>
