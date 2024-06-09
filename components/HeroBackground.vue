fde1fc73febebe767dc4cfb3aa692ec952f1b508
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-06-09 03:15:17 +0200

    chore: increase top bar brightness

--- components/HeroBackground.vue
+++ components/HeroBackground.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-full">
     <div class="absolute top-0 left-0 w-full custom-gradient-height bg-gradient-to-b from-primary to-transparent opacity-20 dark:opacity-15" />
-    <div class="absolute top-0 left-0 w-full h-[2px] bg-gradient-to-r from-transparent via-primary to-transparent opacity-85" />
+    <div class="absolute top-0 left-0 w-full h-[2px] bg-gradient-to-r from-transparent via-primary to-transparent opacity-100" />
     <div class="relative z-10 pt-[2px]" />
   </div>
 </template>
