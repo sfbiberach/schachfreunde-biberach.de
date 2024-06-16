5f904ca45e8316a8ecc9537eb26c754fa446a10a
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-06-16 15:46:39 +0200

    chore: add card, cardgroup to components

--- plugins/components.ts
+++ plugins/components.ts
@@ -1,6 +1,8 @@
-import { NuxtImg } from '#components'
+import { Card, CardGroup, NuxtImg } from '#components'
 
 // Add them to main entry (useful for content usage)
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('NuxtImg', NuxtImg)
+  nuxtApp.vueApp.component('Card', Card)
+  nuxtApp.vueApp.component('CardGroup', CardGroup)
 })
