de13b8cd1085f833260c1efcaf1f6703a03a2210
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-06-16 17:49:29 +0200

    chore: enable nuxthub

--- plugins/components.ts
+++ plugins/components.ts
@@ -1,8 +1,6 @@
-import { Card, CardGroup, NuxtImg } from '#components'
+import { NuxtImg } from '#components'
 
 // Add them to main entry (useful for content usage)
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('NuxtImg', NuxtImg)
-  nuxtApp.vueApp.component('Card', Card)
-  nuxtApp.vueApp.component('CardGroup', CardGroup)
 })
