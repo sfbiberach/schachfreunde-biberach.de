be1ec37845ef49731ee26aff80887b7d71a1c4ee
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2024-06-17 03:02:10 +0200

    feat: add img-carousel component

---
+++ components/content/ImgCarousel.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  ui: {
+    type: Object,
+  },
+})
+</script>
+
+<template>
+  <UCarousel v-slot="{ item }" :items="props.items" :ui="{ container: 'gap-4', item: '' }" arrows indicators class="not-prose" :prev-button="{ class: 'disabled:hidden' }" :next-button="{ class: 'disabled:hidden' }">
+    <img v-bind="item" draggable="false" :class="{ ui }" class="h-80 max-w-fit">
+  </UCarousel>
+</template>
