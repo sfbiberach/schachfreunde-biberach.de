f43e15f31b7c15a440deb0768afbe003657be586
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-01-04 15:04:11 +0100

    feat(index): add training and contact section

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
 const { data: page } = await useAsyncData('/', () => queryContent('/').findOne())
 
 definePageMeta({
-  heroBackground: 'opacity-100',
+  heroBackground: 'opacity-80',
 })
 
 useSeoMeta({
@@ -44,6 +44,48 @@ const images = [
       <UPageGrid v-if="page.cards">
         <ULandingCard v-for="(card, index) in page.cards" :key="index" v-bind="card" />
       </UPageGrid>
+    </ULandingSection>
+
+    <ULandingSection
+      id="training"
+      title="Termine & Trainingszeiten"
+      align="right"
+    >
+      <template #description>
+        <div class="prose prose-primary dark:prose-invert text-lg">
+          <h3>Wöchentliches Training</h3>
+          <p>Wir treffen uns jeden Freitag um 18:00 Uhr zum Jugendtraining in unserem Spiellokal. Anschließend findet um 20:15 Uhr das Erwachsenentraining statt. Schau doch gerne mal vorbei, eine Voranmeldung ist nicht nötig!</p>
+          <h3>Adresse</h3>
+          <p>
+            Grundschule Biberach<br>
+            Bibersteige 9<br>
+            74078 Heilbronn-Biberach<br>
+            2. Stock, Vereinseingang<br>
+            (neben Hallenbad)
+          </p>
+        </div>
+      </template>
+      <iframe class="rounded-xl w-full h-full min-h-[384px]" scrolling="yes" src="https://calendar.google.com/calendar/embed?src=sf.hnbiberach%40gmail.com&amp;mode=AGENDA&amp;showTitle=0&amp;showDate=0&amp;showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0" />
+    </ULandingSection>
+
+    <ULandingSection>
+      <ULandingCTA
+        title="Kontaktiere uns"
+        align="left"
+        :card="false"
+      >
+        <template #description>
+          <p class="prose prose-primary dark:prose-invert text-lg">
+            Falls Du uns näher kennenlernen möchtest, kannst Du entweder in unserem Spiellokal vorbeischauen (wie oben beschrieben) oder uns per Mail/WhatsApp kontaktieren. Suche Dir dazu die <NuxtLink to="/impressum">
+              Kontaktdaten
+            </NuxtLink> eines geeigneten Ansprechpartners und schreibe uns eine Nachricht. Wir freuen uns!
+          </p>
+        </template>
+        <img
+          src="https://picsum.photos/640/360"
+          class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
+        >
+      </ULandingCTA>
     </ULandingSection>
   </NuxtLayout>
 </template>
