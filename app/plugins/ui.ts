2adef94ab7c9f4ce73d77cfaf55901cc565499a5
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-05-29 11:53:10 +0200

    refactor: update ui plugin for ui 3

--- app/plugins/ui.ts
+++ app/plugins/ui.ts
@@ -1,61 +1,81 @@
-import colors from '#tailwind-config/theme/colors'
-
-function hexToRgb(hex: string) {
-  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
-  hex = hex.replace(shorthandRegex, (_, r, g, b) => {
-    return r + r + g + g + b + b
-  })
-
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return result && result[1] && result[2] && result[3]
-    ? `${Number.parseInt(result[1], 16)} ${Number.parseInt(result[2], 16)} ${Number.parseInt(result[3], 16)}`
-    : null
-}
-
 export default defineNuxtPlugin({
   enforce: 'post',
   setup() {
     const appConfig = useAppConfig()
 
-    const root = computed(() => {
-      const primary: Record<string, string> | undefined = (colors as any)[appConfig.ui.primary]
-      const gray: Record<string, string> | undefined = (colors as any)[appConfig.ui.gray]
-
-      return `:root {
-        ${Object.entries(primary || colors.green).map(([key, value]) => `--color-primary-${key}: ${hexToRgb(value)};`).join('\n')}
-        --color-primary-DEFAULT: var(--color-primary-500);
-
-        ${Object.entries(gray || colors.cool).map(([key, value]) => `--color-gray-${key}: ${hexToRgb(value)};`).join('\n')}
-        }
-
-        .dark {
-          --color-primary-DEFAULT: var(--color-primary-400);
-        }
-        `
-    })
-
     if (import.meta.client) {
-      watch(root, () => {
-        window.localStorage.setItem('nuxt-ui-root', root.value)
-      })
+      function updateColor(type: 'primary' | 'neutral') {
+        const color = localStorage.getItem(`nuxt-ui-${type}`)
+        if (color) {
+          appConfig.ui.colors[type] = color
+        }
+      }
 
-      appConfig.ui.primary = window.localStorage.getItem('nuxt-ui-primary') || appConfig.ui.primary
-      appConfig.ui.gray = window.localStorage.getItem('nuxt-ui-gray') || appConfig.ui.gray
+      function updateRadius() {
+        const radius = localStorage.getItem('nuxt-ui-radius')
+        if (radius && appConfig?.theme) {
+          appConfig.theme.radius = Number.parseFloat(radius)
+        }
+      }
+
+      function updateBlackAsPrimary() {
+        const blackAsPrimary = localStorage.getItem('nuxt-ui-black-as-primary')
+        if (blackAsPrimary && appConfig?.theme) {
+          appConfig.theme.blackAsPrimary = blackAsPrimary === 'true'
+        }
+      }
+
+      updateColor('primary')
+      updateColor('neutral')
+      updateRadius()
+      updateBlackAsPrimary()
     }
+
     if (import.meta.server) {
       useHead({
-        script: [
-          {
-            innerHTML: `
-                if (localStorage.getItem('nuxt-ui-root')) {
-                  document.querySelector('style#nuxt-ui-colors').innerHTML = localStorage.getItem('nuxt-ui-root')
-                }`.replace(/\s+/g, ' '),
-            type: 'text/javascript',
-            tagPriority: -1,
-          },
-        ],
+        script: [{
+          innerHTML: `
+            let html = document.querySelector('style#nuxt-ui-colors').innerHTML;
+
+            if (localStorage.getItem('nuxt-ui-primary')) {
+              const primaryColor = localStorage.getItem('nuxt-ui-primary');
+              if (primaryColor !== 'black') {
+                html = html.replace(
+                  /(--ui-color-primary-\\d{2,3}:\\s*var\\()--color-${appConfig.ui.colors.primary}-(\\d{2,3}\\))/g,
+                  \`$1--color-\${primaryColor}-$2\`
+                );
+              }
+            }
+            if (localStorage.getItem('nuxt-ui-neutral')) {
+              let neutralColor = localStorage.getItem('nuxt-ui-neutral');
+              html = html.replace(
+                /(--ui-color-neutral-\\d{2,3}:\\s*var\\()--color-${appConfig.ui.colors.neutral}-(\\d{2,3}\\))/g,
+                \`$1--color-\${neutralColor === 'neutral' ? 'old-neutral' : neutralColor}-$2\`
+              );
+            }
+
+            document.querySelector('style#nuxt-ui-colors').innerHTML = html;
+            `.replace(/\s+/g, ' '),
+          type: 'text/javascript',
+          tagPriority: -1
+        }, {
+          innerHTML: `
+            if (localStorage.getItem('nuxt-ui-radius')) {
+              document.querySelector('style#nuxt-ui-radius').innerHTML = ':root { --ui-radius: ' + localStorage.getItem('nuxt-ui-radius') + 'rem; }';
+            }
+          `.replace(/\s+/g, ' '),
+          type: 'text/javascript',
+          tagPriority: -1
+        }, {
+          innerHTML: `
+            if (localStorage.getItem('nuxt-ui-black-as-primary') === 'true') {
+              document.querySelector('style#nuxt-ui-black-as-primary').innerHTML = ':root { --ui-primary: black; } .dark { --ui-primary: white; }';
+            } else {
+              document.querySelector('style#nuxt-ui-black-as-primary').innerHTML = '';
+            }
+          `.replace(/\s+/g, ' ')
+        }]
       })
     }
-  },
+  }
 })
