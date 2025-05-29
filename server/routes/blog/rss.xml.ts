395d0abee1e1effe1330cfb15f63798e83923cac
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-05-29 11:56:47 +0200

    refactor: update rss for content 3

--- server/routes/blog/rss.xml.ts
+++ server/routes/blog/rss.xml.ts
@@ -1,7 +1,10 @@
-import { serverQueryContent } from '#content/server'
+import type { CollectionQueryBuilder, Collections } from '@nuxt/content'
+import type { H3Event } from 'h3'
+import { queryCollection } from '#imports'
 import { Feed } from 'feed'
 import { joinURL } from 'ufo'
-import { BLOG_PATHS } from '~~/constants/blog'
+
+type queryCollectionWithEvent = <T extends keyof Collections>(event: H3Event, collection: T) => CollectionQueryBuilder<Collections[T]>
 
 export default defineEventHandler(async (event) => {
   const baseUrl = 'http://sf-biberach.nuxt.space'
@@ -20,20 +23,24 @@ export default defineEventHandler(async (event) => {
     },
   })
 
-  const articles = await serverQueryContent(event, BLOG_PATHS.BASE)
-    .sort({ date: -1 })
-    .where({ _partial: false, _draft: false, _type: 'markdown' })
-    .find()
+  const articles = await (queryCollection as queryCollectionWithEvent)(event, 'blog')
+    .order('date', 'DESC')
+    .all()
 
   for (const article of articles) {
+    if (article.draft) {
+      continue
+    }
     feed.addItem({
-      link: joinURL(baseUrl, article._path ?? ''),
-      image: joinURL(baseUrl, article.image ?? ''),
-      title: article.title ?? '',
+      link: joinURL(baseUrl, article.path),
+      // image: joinURL(baseUrl, article.image),
+      title: article.title,
       date: new Date(article.date),
       description: article.description,
-      author: article.authors,
-      category: article.category,
+      category: [{
+        name: article.category,
+      }],
+      // author: article.authors, INF0: Cannot work without an email field in the author object https://github.com/jpmonette/feed/issues/141
     })
   }
 
