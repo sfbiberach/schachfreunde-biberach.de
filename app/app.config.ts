15c42951b0955be9b2d1654eb9eb8dd812bc5a6c
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-05-29 11:50:31 +0200

    refactor(footer): use components from ui-base

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,180 @@
+import type { ButtonProps } from '#ui/types'
+
 export default defineAppConfig({
+  app: {
+    meta: {
+      copyright: {
+        copyrightYear: new Date().getFullYear(),
+        copyrightHolder: 'happydesigns',
+        copyrightHomepage: 'https://happydesigns.de',
+      },
+      socials: [
+        {
+          icon: 'i-simple-icons-instagram',
+          color: 'neutral' as const,
+          variant: 'ghost' as const,
+          to: 'https://www.instagram.com/schachfreundeheilbronnbiberach',
+          target: '_blank',
+        },
+        {
+          icon: 'i-simple-icons-facebook',
+          color: 'neutral' as const,
+          variant: 'ghost' as const,
+          to: 'https://www.facebook.com/Schachfreunde.HN.Biberach',
+          target: '_blank',
+        },
+        {
+          icon: 'i-simple-icons-github',
+          color: 'neutral' as const,
+          variant: 'ghost' as const,
+          to: 'https://github.com/sfbiberach',
+          target: '_blank',
+        },
+      ] as ButtonProps[],
+    },
+
+    links: {
+      footer: [
+        {
+          label: 'Homepage',
+          to: '/',
+          children: [
+            {
+              label: 'Startseite',
+              to: '/',
+              icon: 'i-ph-house-duotone',
+            },
+            {
+              label: 'Blog',
+              to: '/blog',
+              icon: 'i-ph-newspaper',
+            },
+            {
+              label: 'Mannschaften',
+              to: '/mannschaften',
+              icon: 'i-ph-castle-turret-duotone',
+            },
+            {
+              label: 'Turniere',
+              to: '/turniere',
+              icon: 'i-ph-trophy-duotone',
+            },
+          ],
+        },
+        {
+          label: 'Rechtliches',
+          to: '/',
+          children: [
+            {
+              label: 'Impressum',
+              to: '/impressum',
+              icon: 'i-ph-file-text-duotone',
+            },
+            {
+              label: 'Datenschutz',
+              to: '/datenschutz',
+              icon: 'i-ph-shield-duotone',
+            },
+          ],
+        },
+        {
+          label: 'Externe Links',
+          to: '/',
+          children: [
+            {
+              label: 'Unterländer Schachtage',
+              to: 'https://www.unterlaender-schachtage.de/',
+              target: '_blank',
+              icon: 'i-ph-crown-duotone',
+            },
+
+            {
+              label: 'Aktuelle DWZ-Liste',
+              to: 'https://schach.in/zahlen/C0652/',
+              target: '_blank',
+              icon: 'i-ph-chart-bar-duotone',
+            },
+            {
+              label: 'Google Vereinskalender',
+              to: 'https://calendar.google.com/calendar/embed?src=dimi.triantafillidis%40web.de&ctz=Europe%2FBerlin',
+              target: '_blank',
+              icon: 'i-ph-calendar-duotone',
+            },
+          ],
+        },
+      ],
+    },
+
+    blog: {
+      categories: {
+        Jugend: {
+          label: 'Jugend',
+          color: 'orange',
+        },
+        Mannschaft: {
+          label: 'Mannschaft',
+          color: 'green',
+        },
+        Verein: {
+          label: 'Verein',
+          color: 'blue',
+        },
+      },
+    },
+
+    chessboard: {
+      summon: {
+        chance: 0.15,
+        limit: 2,
+      },
+      pieces: [
+        {
+          icon: 'i-fa6-solid-chess-pawn',
+          moves: {
+            step: [[0, 1]],
+            start: [[0, 1], [0, 2]],
+            capture: [[1, 1]],
+          },
+        },
+        {
+          icon: 'i-fa6-solid-chess-knight',
+          moves: {
+            step: [[2, 1], [1, 2]],
+          },
+        },
+        {
+          icon: 'i-fa6-solid-chess-bishop',
+          moves: {
+            step: [[1, 1], [2, 2], [3, 3]],
+          },
+        },
+        {
+          icon: 'i-fa6-solid-chess-rook',
+          moves: {
+            step: [[0, 1], [0, 2], [0, 3], [1, 0], [2, 0], [3, 0]],
+          },
+        },
+        {
+          icon: 'i-fa6-solid-chess-queen',
+          moves: {
+            step: [[0, 1], [0, 2], [0, 3], [1, 0], [2, 0], [3, 0], [1, 1], [2, 2], [3, 3]],
+          },
+        },
+        {
+          icon: 'i-fa6-solid-chess-king',
+          moves: {
+            step: [[0, 1], [1, 0], [1, 1]],
+          },
+        },
+      ],
+    },
+  },
+
   ui: {
-    primary: 'blue',
-    gray: 'neutral',
+    colors: {
+      primary: 'blue',
+      neutral: 'neutral',
+    },
     icons: {
       light: 'i-ph-sun-duotone',
       dark: 'i-ph-moon-duotone',
@@ -171,70 +344,15 @@ export default defineAppConfig({
       },
     },
   },
-  app: {
-    blog: {
-      categories: {
-        Jugend: {
-          label: 'Jugend',
-          color: 'orange',
-        },
-        Mannschaft: {
-          label: 'Mannschaft',
-          color: 'green',
-        },
-        Verein: {
-          label: 'Verein',
-          color: 'blue',
-        },
+
+  uiPro: {
+    footer: {
+      slots: {
+        top: 'border-b border-default',
       },
-    },
-    chessboard: {
-      summon: {
-        chance: 0.15,
-        limit: 2,
-      },
-      pieces: [
-        {
-          icon: 'i-fa6-solid-chess-pawn',
-          moves: {
-            step: [[0, 1]],
-            start: [[0, 1], [0, 2]],
-            capture: [[1, 1]],
-          },
-        },
-        {
-          icon: 'i-fa6-solid-chess-knight',
-          moves: {
-            step: [[2, 1], [1, 2]],
-          },
-        },
-        {
-          icon: 'i-fa6-solid-chess-bishop',
-          moves: {
-            step: [[1, 1], [2, 2], [3, 3]],
-          },
-        },
-        {
-          icon: 'i-fa6-solid-chess-rook',
-          moves: {
-            step: [[0, 1], [0, 2], [0, 3], [1, 0], [2, 0], [3, 0]],
-          },
-        },
-        {
-          icon: 'i-fa6-solid-chess-queen',
-          moves: {
-            step: [[0, 1], [0, 2], [0, 3], [1, 0], [2, 0], [3, 0], [1, 1], [2, 2], [3, 3]],
-          },
-        },
-        {
-          icon: 'i-fa6-solid-chess-king',
-          moves: {
-            step: [[0, 1], [1, 0], [1, 1]],
-          },
-        },
-      ],
     },
   },
+
   links: {
     header: [{
       label: 'Blog',
--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,43 +1,22 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
-
-const { data: footerTexts } = await useAsyncData('footer', () => queryContent('_footer').find())
+const footerButtons = appConfig?.app?.meta?.socials
 </script>
 
 <template>
-  <UFooter
-    :ui="{
-      bottom: { wrapper: 'border-t border-gray-200 dark:border-gray-800', container: '!py-6', left: '~', center: '~', right: '~' },
-      top: { wrapper: 'border-t border-gray-200 dark:border-gray-800', container: 'py-8 lg:py-12' },
-    }"
-  >
+  <USeparator icon="i-ph-crown-cross-duotone" class="h-px" />
+
+  <UFooter>
     <template #top>
-      <UFooterColumns :links="appConfig.links.footer" :ui="{ wrapper: 'xl:grid-cols-5', center: `xl:col-span-${appConfig.links.footer?.length}`, right: `flex flex-col lg:grid grid-flow-col auto-cols-fr gap-8 lg:grid-cols-${appConfig.links.footer?.length} xl:grid-cols-none col-span-2` }">
-        <template #right>
-          <NewsletterForm class="space-y-6" />
-          <div v-for="(text, index) in footerTexts" :key="index" class="space-y-6 text-sm">
-            <h3 class="text-sm/6 font-semibold text-gray-900 dark:text-white">
-              {{ text.title }}
-            </h3>
-            <ContentRenderer :value="text" class="prose prose-primary dark:prose-invert max-w-none text-sm" />
-          </div>
-        </template>
-      </UFooterColumns>
+      <UContainer>
+        <HFooterColumns />
+      </UContainer>
     </template>
     <template #left>
-      <p class="text-gray-500 dark:text-gray-400 text-sm">
-        Copyright © {{ new Date().getFullYear() }}
-        <NuxtLink to="https://www.happydesigns.de/" target="_blank">
-          happydesigns
-        </NuxtLink>
-      </p>
+      <HFooterCopyright />
     </template>
     <template #right>
-      <div class="-ml-[0.375rem]">
-        <UButton aria-label="Sfr. HN-Biberach auf Instagram" to="https://www.instagram.com/schachfreundeheilbronnbiberach/" target="_blank" icon="i-simple-icons-instagram" v-bind="($ui.button.secondary as any)" />
-        <UButton aria-label="Sfr. HN-Biberach auf Facebook" to="https://www.facebook.com/Schachfreunde.HN.Biberach/" target="_blank" icon="i-simple-icons-facebook" v-bind="($ui.button.secondary as any)" />
-        <UButton aria-label="Sfr. HN-Biberach auf GitHub" to="https://github.com/sfbiberach" target="_blank" icon="i-simple-icons-github" v-bind="($ui.button.secondary as any)" />
-      </div>
+      <HFooterButtons :buttons="footerButtons" />
     </template>
   </UFooter>
 </template>
