4328cddab598cff2c1fa6ea4ff9e939d75319189
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-09 06:46:15 +0200

    refactor: rename usePrimaryColor file

--- app/composables/usePrimaryColor.ts
+++ app/composables/usePrimaryColor.ts
@@ -2,9 +2,9 @@ export function usePrimaryColor(color?: string) {
   const appConfig = useAppConfig()
 
   if (color) {
-    appConfig.ui.primary = color
+    appConfig.ui.colors.primary = color
     window.localStorage.setItem('nuxt-ui-primary', color)
   }
 
-  return appConfig.ui.primary
+  return appConfig.ui.colors.primary
 }
