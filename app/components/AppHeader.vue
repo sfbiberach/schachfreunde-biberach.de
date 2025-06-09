a1b5a95934c906fb0d2b9bfc30c6d3c053821ca1
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-09 06:37:57 +0200

    refactor: update AppHeader for v3

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
 const appConfig = useAppConfig()
+const items = appConfig.app.links.header
 </script>
 
 <template>
-  <UHeader class="page-header" :links="appConfig.links.header">
-    <template #logo>
+  <UHeader class="page-header">
+    <template #title>
       <div class="flex gap-2 items-center">
         <UIcon name="i-ph-horse-duotone" class="text-3xl" />
-        <!-- <Logo class="h-7" /> -->
         SF HN-Biberach
       </div>
     </template>
+
+    <UNavigationMenu :items variant="link" :ui="{ linkLeadingIcon: 'hidden' }" />
 
     <template #right>
       <UTooltip :text="$colorMode.preference === 'dark' ? 'Zu hellem Modus wechseln' : 'Zu dunklem Modus wechseln'" :popper="{ strategy: 'absolute' }">
@@ -19,6 +21,10 @@ const appConfig = useAppConfig()
       <UTooltip text="Suche" :kbds="['meta', 'K']" :popper="{ strategy: 'absolute' }">
         <UContentSearchButton label="" />
       </UTooltip>
+    </template>
+
+    <template #body>
+      <UNavigationMenu :items orientation="vertical" />
     </template>
   </UHeader>
 </template>
