43c18d8b96dd3b61113c42c8c52b46f3fa4fec28
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-24 02:21:21 +0200

    feat(tournament): update overview page for content v3

--- app/pages/turniere/index.vue
+++ app/pages/turniere/index.vue
@@ -3,36 +3,25 @@ definePageMeta({
   heroBackground: 'opacity-70',
 })
 
-const { tournaments, fetchList } = useTournaments()
-
-await fetchList()
+const { data: tournaments } = await useTournaments()
 </script>
 
 <template>
-  <NuxtLayout
-    title="Turniere"
-    description="Hier bekommst Du einen Überblick über die Schachturniere, die wir selbst ausrichten. Von Opens mit mehreren hundert Teilnehmern bis hin zu kleineren Turnieren ist für jeden etwas dabei."
-    align="center"
-    :container="true"
-    :prose="false"
-  >
+  <NuxtLayout name="content">
     <UPageGrid class="mt-8">
       <UPageCard
         v-for="(tournament, index) in tournaments"
         :key="index"
-        :to="tournament.external ? tournament.link : tournament._path"
-        :title="tournament.title"
-        :description="tournament.description"
+        v-bind="tournament"
+        :to="tournament.path"
+        variant="subtle"
         :ui="{
-          divide: '',
-          footer: { padding: 'pt-0' },
           title: 'text-lg',
           description: 'line-clamp-3',
         }"
-        :target="tournament.external ? '_blank' : '_self'"
       >
-        <template v-if="tournament.date" #footer>
-          <UBadge :label="getTournamentDate(tournament)" color="gray" />
+        <template v-if="tournament?.date" #footer>
+          <UBadge :label="toArray(tournament?.date).map(d => formatDate(d)).join(' - ')" color="neutral" variant="subtle" />
         </template>
       </UPageCard>
     </UPageGrid>
