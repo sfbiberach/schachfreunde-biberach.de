38ba2ba3f773c556d56a8e47263dd11e30aa6eda
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-24 02:12:27 +0200

    refacor(tournament): update query composables to content v3

--- app/composables/useTournaments.ts
+++ app/composables/useTournaments.ts
@@ -1,27 +1,6 @@
-import type { Tournament } from '~/types'
-
 export function useTournaments() {
-  const tournaments = useState<Tournament[]>('tournaments', () => [])
-
-  // Data fetching
-  async function fetchList() {
-    if (tournaments.value.length) {
-      return
-    }
-
-    try {
-      const data = await queryContent('/turniere').where({ _extension: 'md' }).find() as Tournament[]
-
-      tournaments.value = data
-    }
-    catch (e) {
-      tournaments.value = []
-      return e
-    }
-  }
-
-  return {
-    fetchList,
-    tournaments,
-  }
+  return useAsyncData(
+    'tournament',
+    () => queryCollection('tournament').all(),
+  )
 }
