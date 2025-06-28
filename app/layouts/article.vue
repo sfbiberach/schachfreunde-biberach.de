edf9ea0062b80233199c056749a80722af408b25
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-06-29 01:04:35 +0200

    fix(article): padding around ContentToc

--- app/layouts/article.vue
+++ app/layouts/article.vue
@@ -92,7 +92,7 @@ const badge = computed(() => {
           <UContentToc
             title="Inhaltsverzeichnis"
             :links="page?.body?.toc?.links"
-            :ui="{ container: page?.body?.toc?.links?.length ? '' : 'border-none p-0!' }"
+            :ui="{ container: page?.body?.toc?.links?.length ? '' : 'border-none' }"
           >
             <template v-if="props.links" #bottom>
               <USeparator v-if="page?.body?.toc?.links?.length" type="dashed" class="py-2 hidden lg:block" />
