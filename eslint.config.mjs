d5ad02e8f5ebd1d8b777251a6f4ac1d5b8def8cf
Jan Fröhlich <jan.froehlich.janf@gmail.com>
2025-12-15 14:55:29 +0100

    chore: add mdclint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import antfu from '@antfu/eslint-config'
+import { mdcLint } from 'mdclint'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
@@ -28,5 +29,10 @@ export default withNuxt(
         'style/brace-style': ['error', 'stroustrup'],
       },
     },
-  ),
+  ).append(await mdcLint({
+    files: ['content/**/*.md'],
+    preset: 'mdc',
+    // @ts-ignore
+    config: { MD013: { line_length: 2048 } },
+  })),
 )
